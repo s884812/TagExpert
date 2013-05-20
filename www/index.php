669ecb5d2a52f0bcf14bafc6bd4ee6f7d772a5a4
FreedomKnight <j26733514@gmail.com>
2013-05-20 23:45:42 +0800

    change folder

--- www/index.php
+++ www/index.php
@@ -5,8 +5,17 @@
         require_once(strtolower($class) . '.php');
     }
 
+    $include_path = array();
+    $include_path[] = get_include_path();
+    $include_path[] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'core';
+    $include_path[] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'model';
+    $include_path[] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'view';
+    $include_path[] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'control';
+    set_include_path(join(PATH_SEPARATOR, $include_path));
     header('Content-type: text/html;charset=utf-8');
+
     $action = new Action();
+    $action->setRouter(new Router());
     $action->run();
 ?>
 
