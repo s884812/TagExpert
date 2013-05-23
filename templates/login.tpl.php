ac5bf476c740f1b6d18e1ef3406d4689c1415542
FreedomKnight <j26733514@gmail.com>
2013-05-24 00:03:45 +0800

    add bootstrap

--- templates/login.tpl.php
+++ templates/login.tpl.php
@@ -1,13 +1,18 @@
 <html>
+    <!DOCTYPE html>
+    
 <head>
     <script data-main="js/apps/login/main" src="js/lib/require.js"></script>
+    <style> @import url(css/bootstrap/bootstrap.css) </style>
 </head>
 <body>
+</div class="container-fluid">
     <form method="post" action="index.php?act=login">
-        帳號：<input type="text" name="account" id="account" /><br />
+        帳號：<input type="text" class= name="account" id="account" /><br />
         密碼：<input type="password" name="password" id="password" /><br />
-        <input type="submit" value="確定送出" />
+        <input type="submit" class="btn btn-primary" value="確定送出" />
     </form>
     <p id="messege"> </p>
+</div>
 </body>
 </html>
