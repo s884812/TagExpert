dc7a1992a1f3837efc3b8ae46f9d1a6383ab1fdf
FreedomKnight <j26733514@gmail.com>
2013-05-24 03:25:35 +0800

    index nav

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -4,8 +4,17 @@
     <style type="text/css"> @import url(css/bootstrap/bootstrap.css); </style>
     <style type="text/css"> @import url(css/index/index.css); </style>-
 </head>
-
 <body>
+    <div class="navbar">
+      <div class="navbar-inner">
+        <a class="brand" href="#">Title</a>
+        <ul class="nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div>
+    </div>
     <div class="row-fluid">
         <div class="span12">
              <div class="row-fluid">
@@ -43,6 +52,18 @@
                  </div>
             </div>
         </div>
+    </div>
+
+    <div class="pagination">
+      <ul>
+        <li><a href="#">Prev</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a href="#">Next</a></li>
+      </ul>
     </div>
 </body> 
 </html>
