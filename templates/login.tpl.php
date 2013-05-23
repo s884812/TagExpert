4dfc29c3920ae77e6b48beb77d538369aa56dbf7
FreedomKnight <j26733514@gmail.com>
2013-05-24 00:25:40 +0800

    change login interface

--- templates/login.tpl.php
+++ templates/login.tpl.php
@@ -1,15 +1,18 @@
+<!DOCTYPE html>
 <html>
-    <!DOCTYPE html>
     
 <head>
     <script data-main="js/apps/login/main" src="js/lib/require.js"></script>
     <style> @import url(css/bootstrap/bootstrap.css) </style>
+    <style> @import url(css/login/login.css) </style>
 </head>
 <body>
-</div class="container-fluid">
+<div class="container-fluid">
     <form method="post" action="index.php?act=login">
-        帳號：<input type="text" class= name="account" id="account" /><br />
-        密碼：<input type="password" name="password" id="password" /><br />
+        <label> 帳號：</label>
+        <input type="text" placeholder="請輸入帳號" name="account" id="account" /><br />
+        <label>密碼：</label>
+        <input type="password" placeholder="請輸入密碼" name="password" id="password" /><br />
         <input type="submit" class="btn btn-primary" value="確定送出" />
     </form>
     <p id="messege"> </p>
