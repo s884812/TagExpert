09a04533d8fdc5019bf176e189e754ae3deeb86d
FreedomKnight <j26733514@gmail.com>
2013-05-25 00:48:16 +0800

    use table tag let the topic have border framework

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -6,51 +6,49 @@
 </head>
 <body>
     <div class="navbar">
-      <div class="navbar-inner">
-        <a class="brand" href="#">Title</a>
-        <ul class="nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-        </ul>
-      </div>
+        <div class="navbar-inner">
+            <a class="brand" href="#">TagExpert</a>
+                <ul class="nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+        </div>
     </div>
     <div class="row-fluid">
-        <div class="span12">
-             <div class="row-fluid">
-                 <div class="span6 offset2" id="topic">
+        <div class="span6 offset2">
 <?php
     foreach( $this->tplVar as $row ) {
-        echo '                <div>' . "\n";
-        echo '                    <h2>'. $row['title'] . "</h2>\n";
-        echo '                    <hr/>' . "\n";
-        echo '                    <div id="box"" >' .  $row['content'] . '</div><br/>' . "\n";
-        echo '                </div>' . "\n";
+        echo '                <table id="topic" class="table table-bordered">' . "\n";
+        echo '                    <tr>' . "\n";
+        echo '                        <td><h3>'. $row['title'] . "</h3></td>\n";
+        echo '                    </tr>' ."\n";
+        echo '                    <tr>' ."\n";
+        echo '                        <td><div>' .  $row['content'] . '</div></td>' . "\n";
+        echo '                    </tr>' . "\n";
+        echo '                </table>' . "\n";
     }
 ?>
      
-                 </div >
-            <div class="row-fluid">
-                 <div class="span2 offset1" id="control">
-                     <table class = "table table-striped table-bordered" >
-                         <tr>
-                             <td>已訂閱標籤列表</td>
-                         </tr>
-                         <tr>
-                             <td>標籤一</td>
-                         </tr>
-                         <tr>
-                             <td>標籤二</td>
-                         </tr>
-                         <tr>
-                             <td>標籤三</td>
-                         </tr>
-                     </table>
-                     <form >
-                         <button type="button" class="btn-large btn-primary" id="posting" onclick="window.location='index.php?act=edit'"> 發文 </button>
-                     </form>
-                 </div>
-            </div>
+        </div >
+        <div class="span2 offset1" id="control">
+             <table class = "table table-striped table-bordered" >
+                 <tr>
+                     <td>已訂閱標籤列表</td>
+                 </tr>
+                 <tr>
+                     <td>標籤一</td>
+                 </tr>
+                 <tr>
+                     <td>標籤二</td>
+                 </tr>
+                 <tr>
+                     <td>標籤三</td>
+                 </tr>
+             </table>
+             <form >
+                 <button type="button" class="btn-large btn-primary" id="posting" onclick="window.location='index.php?act=edit'"> 發文 </button>
+             </form>
         </div>
     </div>
 
